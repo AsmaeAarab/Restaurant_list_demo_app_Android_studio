8e4109043f9d926629375a826148f5456d9e2258
asmae aarab <asmae.aarab98@gmail.com>
2020-12-13 14:04:37 +0000

    categories added

---
+++ app/src/main/java/com/example/tprestaurant/Restaurant_Liste.java
@@ -0,0 +1,14 @@
+package com.example.tprestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Restaurant_Liste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant__liste);
+    }
+}
